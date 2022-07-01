5c9f3c46bdb7baf82daeede19aa65d51f24377e7
georgenicole <georgenicole1402@gmail.com>
2022-07-01 16:01:17 -0300

    it has added the path of itemDetail and show as well

--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
-export const ItemDetail = ({product}) => {
-    console.log(product);
+export const ItemDetail = ({ product }) => {
   return (
     <div>
-        <h1>{product.title}</h1>
-        <img src={product.image} alt='imageproducts'/>
-        <p>{product.price}</p>
+      <h1>{product.title}</h1>
+      <img src={product.image} alt="imageproducts" />
+      <h6>{`Price: $ ${product.price}`}</h6>
+      <section>
+        <p>{product.description}</p>
+      </section>
     </div>
-  )
-}
+  );
+};
--- src/Containers/ItemDetailContainer/ItemDetailContainer.js
+++ src/Containers/ItemDetailContainer/ItemDetailContainer.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { ItemDetail } from '../../Components/ItemDetail/ItemDetail'
-import { baseUrl } from '../../constantes'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { ItemDetail } from "../../Components/ItemDetail/ItemDetail";
+import { baseUrl } from "../../constantes";
 
 export const ItemDetailContainer = () => {
+  let { id } = useParams();
+  const [productDetail, setProductDetail] = useState({});
+  const [error, setError] = useState("");
 
-  const [productDetail, setProductDetail] = useState({})
-  const [error, setError] = useState("")
-
-  useEffect(()=>{
-    const getProductos = async () =>{
+  useEffect(() => {
+    const getProduct = async () => {
       try {
-        const response = await fetch(`${baseUrl}/products`)
+        const response = await fetch(`${baseUrl}/products/${id}`);
         const data = await response.json();
-        console.log(data)
-        // setProductDetail(data)
+        console.log(data);
+         setProductDetail(data)
       } catch (error) {
-        console.log(error)
-        setError(error.message)
+        console.log(error);
+        setError(error.message);
       }
-    }
-     getProductos();
-  }, [])
+    };
+    getProduct();
+  }, [id]);
 
-  return (
-     <ItemDetail product={productDetail}/>
-  )
-}
+  return <ItemDetail product={productDetail} />;
+};
