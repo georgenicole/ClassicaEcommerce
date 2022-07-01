c47707b9be1a52be2219080e96201ecc704fab66
georgenicole <georgenicole1402@gmail.com>
2022-07-01 15:45:02 -0300

    it has been created the navigation to home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import ItemListContainer from "./Containers/ItemListContainer";
 import NavBar from "./Components/NavBar/NavBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ItemDetailContainer } from "./Containers/ItemDetailContainer/ItemDetailContainer";
 
-
-
-
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer />
+    </>
+  );
+}
 
 function App() {
   return (
- <div>
- <NavBar/>
- <ItemListContainer/>
- <ItemDetailContainer/>
- </div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
