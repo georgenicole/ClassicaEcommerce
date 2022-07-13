1f0aaaa37e171e7289777ac3455ea2eba1f9043c
georgenicole <georgenicole1402@gmail.com>
2022-07-13 16:38:27 -0300

    Ready to send

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,16 +1,9 @@
-import { Button } from "@mui/material";
-import React, { useContext } from "react";
-import { Shop } from "../../Context/ShopContext";
+import React from "react";
+
 import Item from "../item/Item";
 import "./styles.css";
 
 const ItemList = ({ products }) => {
-  const { setEstadoA } = useContext(Shop);
-
-  const handleChangeState = () => {
-    setEstadoA("other value");
-  };
-
   return (
     <div className="itemContainer">
       {products?.map((prod) => (
