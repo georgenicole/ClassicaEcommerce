b3e779828ed9bf0ccf40e0e54c14574967dcefba
georgenicole <georgenicole1402@gmail.com>
2022-07-22 13:42:04 -0300

    REFACTOR: it has modified the display in the homeItems

--- src/Components/HomeItems/HomeItems.js
+++ src/Components/HomeItems/HomeItems.js
@@ -7,18 +7,14 @@ const HomeItems = ({ prod }) => {
   return (
     <div className="homeItems-navbar">
       <Button variant="text">
-        <Link
-          to="/"
-          className="homeItems-navbar_buttons"
-          style={{ color: "#BF9270" }}
-        >
+        <Link to="/" className="homeitems-buttons" style={{ color: "#BF9270" }}>
           Home
         </Link>
       </Button>
       <Button variant="text">
         <Link
           to="/category/breads"
-          className="homeItems-navbar_buttons"
+          className="homeitems-buttons"
           style={{ color: "#BF9270" }}
         >
           Breads
@@ -27,7 +23,7 @@ const HomeItems = ({ prod }) => {
       <Button variant="text">
         <Link
           to="/category/cookies"
-          className="homeItems-navbar_buttons"
+          className="homeitems-buttons"
           style={{ color: "#BF9270" }}
         >
           Cookies
@@ -36,7 +32,7 @@ const HomeItems = ({ prod }) => {
       <Button variant="text">
         <Link
           to="/category/donuts"
-          className="homeItems-navbar_buttons"
+          className="homeitems-buttons"
           style={{ color: "#BF9270" }}
         >
           Donuts
