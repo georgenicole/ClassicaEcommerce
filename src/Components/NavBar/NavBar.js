b1395d7b6835d6d256c5671d64b9fda7b6995c64
georgenicole <georgenicole1402@gmail.com>
2022-07-22 14:03:12 -0300

    REFACTOR: It has removed unused components and changed styles

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useContext } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,18 +6,15 @@ import AvatarImage from "../Avatar/Avatar";
 import HomeItems from "../HomeItems/HomeItems";
 import { LogoTienda } from "../LogoTienda/LogoTienda";
 import CarWidget from "../CarWidget/CarWidget";
-import { Shop } from "../../Context/ShopContext";
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="inherit">
-        <Toolbar style={{ width: "100%", height: 68 }}>
+        <Toolbar className="toobar">
           <LogoTienda />
           <HomeItems />
-
           <CarWidget />
           <AvatarImage />
         </Toolbar>
