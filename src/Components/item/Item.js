33f19174af4a46ca236ae3c178634cdd23d36dbb
georgenicole <georgenicole1402@gmail.com>
2022-07-22 14:16:11 -0300

    REFACTOR: it has removed unused components and inline styles have been modified too

--- src/Components/item/Item.js
+++ src/Components/item/Item.js
@@ -5,35 +5,19 @@ import {
   Typography,
   CardContent,
   CardActions,
-  Grid,
 } from "@mui/material";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import IconButton from "@mui/material/IconButton";
 import "./Styles.css";
 import { useNavigate } from "react-router-dom";
 
 const Item = ({ prod }) => {
   let navigate = useNavigate();
   return (
-    <div
-      className="card"
-      style={{ widtd: "100%", height: "100%", display: "flex" }}
-      onClick={() => navigate(`/detail/${prod.id}`)}
-    >
+    <div className="card" onClick={() => navigate(`/detail/${prod.id}`)}>
       <div className="card__body">
         <Card sx={{ maxWidth: 300, minHeight: 433 }} className="Card">
-          <img
-            src={prod.image}
-            alt="hola"
-            style={{ width: 350, height: 250 }}
-          />
+          <img src={prod.image} alt="imageProduct" className="product-img" />
           <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              className="card__title"
-            >
+            <Typography gutterBottom variant="h5" component="div">
               {prod.title}
             </Typography>
             <Typography variant="body2" color="text.secondary">
@@ -45,22 +29,10 @@ const Item = ({ prod }) => {
               size="small"
               className="card__btn"
               variant="text"
-              style={{ color: "#BF9270", marginRight: 115 }}
+              style={{ color: "#BF9270", backgroundColor: "#FAEEE0" }}
             >
               Add to Cart
             </Button>
-            <IconButton size="small" edge="start">
-              <FavoriteBorderIcon
-                style={{
-                  color: "#BF9270",
-                  backgroundColor: "#FAEEE0",
-                  width: 25,
-                  height: 27,
-                  borderRadius: 9,
-                  marginLeft: 10,
-                }}
-              />
-            </IconButton>
           </CardActions>
         </Card>
       </div>
