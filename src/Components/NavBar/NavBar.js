43f9d074847004e692c0cedcb4279576d2416e79
georgenicole <georgenicole1402@gmail.com>
2022-08-08 11:54:16 -0300

    it has added changes

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -7,16 +7,39 @@ import HomeItems from "../HomeItems/HomeItems";
 import { LogoTienda } from "../LogoTienda/LogoTienda";
 import CarWidget from "../CarWidget/CarWidget";
 import "./Navbar.css";
+import { useContext } from "react";
+import { ProductContext } from "../../Context/ItemContext";
+
 
 export default function NavBar() {
+
+     const { setProductListFiltered, category, productList} = useContext(ProductContext)
+     const handleSearch = (event) =>{
+      let categoryFiltered = productList;
+      if (category) {
+        categoryFiltered  = productList.filter(
+          (producto) => producto.category === category
+        );
+        }
+      let list = []
+      categoryFiltered.forEach((element)=>{
+        if (element.title.toLowerCase().includes(event.target.value.toLowerCase())){
+          list.push(element) 
+         }})
+       setProductListFiltered(list)
+      }
+       
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="inherit">
         <Toolbar className="toobar">
           <LogoTienda />
+          <div className="input-search">
+            <input type="text" placeholder="Search..." onChange={handleSearch}  />
+          </div>
           <HomeItems />
           <CarWidget />
-          <AvatarImage />
+          {/* <AvatarImage /> */}
         </Toolbar>
       </AppBar>
     </Box>
